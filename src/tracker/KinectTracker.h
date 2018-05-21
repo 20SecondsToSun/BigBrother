68a85d0e0284eb8401a550a47dc1b5e33c677142
Maxim Borodin <borodin.ms@phystech.edu>
2018-05-21 22:47:47 +0300

    Merging faceplusplus and kinect

--- src/tracker/KinectTracker.h
+++ src/tracker/KinectTracker.h
@@ -1,22 +1,50 @@
 #pragma once
 #include "ofMain.h"
 #include "Tracker.h"
+#include "ofxKinectForWindows2.h"
 
 namespace bbrother
 {
 	typedef ofPtr<class KinectTracker> KinectTrackerPtr;
 
-	class KinectTracker: public Tracker
+	class KinectTracker : public Tracker
 	{
 	public:
 		KinectTracker();
 		virtual void update() override;
-		virtual void draw() override;	
+		virtual void draw() override;
 
 		virtual void start() override;
 		virtual void stop() override;
 
 		virtual ~KinectTracker();
+		virtual void init(ConfigPtr config) override;
+
+		void sendImage(const ofImage& image);
+		ofImage cropFrame(const ofImage& source, const ofRectangle& sorect);
+
+		ofEvent<ofImage> imageToSend;
+
+	private:
+		int DepthWidth = 512;
+		int DepthHeight = 424;
+
+		int ColorWidth = 1920;
+		int ColorHeight = 1080;
+		int DepthSize = DepthWidth * DepthHeight;
+
+		bool running = false;
+
+		ofxKFW2::Device kinect;
+		ICoordinateMapper* coordinateMapper;
+
+		ofImage bodyIndexImg, foregroundImg;
+		vector<ofVec2f> colorCoords;
+		int bodiesTrackedCount;
+		bool haveAllStreams;
+
+		ofFbo personFbo;
+
+		bool testOneDraw = false;
 	};
 }
-
